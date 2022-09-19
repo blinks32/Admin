c59cdf8f669edefa4bb040cd5c98e0552fe066e2
blinks32 <chndth@gmail.com>
2022-09-19 20:55:14 +0000

    n

--- src/app/pages/customers/customers.page.ts
+++ src/app/pages/customers/customers.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AvatarService } from 'src/app/services/avatar.service';
 
 @Component({
   selector: 'app-customers',
@@ -6,8 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./customers.page.scss'],
 })
 export class CustomersPage implements OnInit {
+  skeletOns: {}[];
+  hideSkeleton: boolean;
+  triphistory: any;
+  hasNoData: boolean;
 
-  constructor() { }
+  constructor(private chatService: AvatarService) { }
+
+
+
+  async ionViewDidEnter() {
+    this.skeletOns = [
+      {},{},{},{}
+    ]
+
+    this.hideSkeleton = true;
+    this.triphistory = (this.chatService.getRiders())
+
+    this.triphistory.subscribe((d)=>{
+      console.log(d);
+      if (d.length == 0){
+        this.hasNoData = true;
+        this.hideSkeleton = false;
+      }else{
+        this.hideSkeleton = false;
+        this.hasNoData = false;
+      }
+  })
+
+  }
 
   ngOnInit() {
   }
--- src/app/pages/drivers/drivers.page.ts
+++ src/app/pages/drivers/drivers.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AvatarService } from 'src/app/services/avatar.service';
 
 @Component({
   selector: 'app-drivers',
@@ -6,8 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./drivers.page.scss'],
 })
 export class DriversPage implements OnInit {
+  skeletOns: {}[];
+  hideSkeleton: boolean;
+  triphistory: any;
+  hasNoData: boolean;
 
-  constructor() { }
+  constructor(private chatService: AvatarService) { }
+
+
+  async ionViewDidEnter() {
+    this.skeletOns = [
+      {},{},{},{}
+    ]
+
+    this.hideSkeleton = true;
+    this.triphistory = (this.chatService.getDrivers())
+
+    this.triphistory.subscribe((d)=>{
+      console.log(d);
+      if (d.length == 0){
+        this.hasNoData = true;
+        this.hideSkeleton = false;
+      }else{
+        this.hideSkeleton = false;
+        this.hasNoData = false;
+      }
+  })
+
+  }
 
   ngOnInit() {
   }
